f7a79d6c802b8d360167e98e0d6e8356e5a01b2a
Johannes <johannestoftbendtsen@gmail.com>
2025-04-05 20:59:04 +0200

    added last year gdp variable
    Added last year's gdp variable. It has no significant effect but its coefficients are positive.

--- multivariate/v1.r
+++ multivariate/v1.r
@@ -15,29 +15,24 @@ df$CAR_1_wins  <- Winsorize(df$`[-1, 1]`, val = quantile(df$`[-1, 1]`, probs = c
 
 #Winsorized
 model1 = lm(CAR_10_wins ~ Cash + Private + CrossBorder + MtoB + Crisis + Margin + 
-            DtoE + Hybrid + Size + Cash_and_Equivalents + TargetAsset + Diversification +
-            Bull_Bear_Spread + 
-            num_prior_mergers, df)
+            DtoE + Hybrid + Size + Cash_and_Equivalents + TargetAsset + 
+            Diversification + Bull_Bear_Spread + num_prior_mergers + gdp_lag1_tgt, df)
 
 model2 = lm(CAR_7_wins ~ Cash + Private + CrossBorder + MtoB + Crisis + Margin + 
             DtoE + Hybrid + Size + Cash_and_Equivalents + TargetAsset + 
-            Diversification + Bull_Bear_Spread + 
-            num_prior_mergers, df)
+            Diversification + Bull_Bear_Spread + num_prior_mergers + gdp_lag1_tgt, df)
 
 model3 = lm(CAR_5_wins ~ Cash + Private + CrossBorder + MtoB + Crisis + Margin + 
-          DtoE + Hybrid + Size + Cash_and_Equivalents + TargetAsset + 
-          Diversification + Bull_Bear_Spread + 
-          num_prior_mergers, df)
+            DtoE + Hybrid + Size + Cash_and_Equivalents + TargetAsset + 
+            Diversification + Bull_Bear_Spread + num_prior_mergers + gdp_lag1_tgt, df)
 
 model4 = lm(CAR_3_wins ~ Cash + Private + CrossBorder + MtoB + Crisis + Margin + 
             DtoE + Hybrid + Size + Cash_and_Equivalents + TargetAsset + 
-            Diversification + Bull_Bear_Spread + 
-            num_prior_mergers, df)
+            Diversification + Bull_Bear_Spread + num_prior_mergers + gdp_lag1_tgt, df)
 
 model5 = lm(CAR_1_wins ~ Cash + Private + CrossBorder + MtoB + Crisis + Margin + 
             DtoE + Hybrid + Size + Cash_and_Equivalents + TargetAsset + 
-            Diversification + Bull_Bear_Spread + 
-            num_prior_mergers, df)
+            Diversification + Bull_Bear_Spread + num_prior_mergers + gdp_lag1_tgt, df)
 
 
 # Trimming to remove the bottom 1% and top 1% CARs - dropping outliers
@@ -59,23 +54,23 @@ df_1 <- df[df$`[-1, 1]` >= quantile(df$`[-1, 1]`, 0.01, na.rm = TRUE) &
 # Run models on trimmed data
 model1_trim <- lm(`[-10, 10]` ~ Cash + Private + CrossBorder + MtoB + Crisis + Margin + DtoE + Hybrid +
                Size + Cash_and_Equivalents + TargetAsset + Diversification +
-               Bull_Bear_Spread + num_prior_mergers, data = df_10)
+               Bull_Bear_Spread + num_prior_mergers + gdp_lag1_tgt, data = df_10)
 
 model2_trim <- lm(`[-7, 7]` ~ Cash + Private + CrossBorder + MtoB + Crisis + Margin + DtoE + Hybrid +
                Size + Cash_and_Equivalents + TargetAsset + Diversification +
-               Bull_Bear_Spread + num_prior_mergers, data = df_7)
+               Bull_Bear_Spread + num_prior_mergers + gdp_lag1_tgt, data = df_7)
 
 model3_trim <- lm(`[-5, 5]` ~ Cash + Private + CrossBorder + MtoB + Crisis + Margin + DtoE + Hybrid +
                Size + Cash_and_Equivalents + TargetAsset + Diversification +
-               Bull_Bear_Spread + num_prior_mergers, data = df_5)
+               Bull_Bear_Spread + num_prior_mergers + gdp_lag1_tgt, data = df_5)
 
 model4_trim <- lm(`[-3, 3]` ~ Cash + Private + CrossBorder + MtoB + Crisis + Margin + DtoE + Hybrid +
                Size + Cash_and_Equivalents + TargetAsset + Diversification +
-               Bull_Bear_Spread + num_prior_mergers, data = df_3)
+               Bull_Bear_Spread + num_prior_mergers + gdp_lag1_tgt, data = df_3)
 
 model5_trim <- lm(`[-1, 1]` ~ Cash + Private + CrossBorder + MtoB + Crisis + Margin + DtoE + Hybrid +
                Size + Cash_and_Equivalents + TargetAsset + Diversification +
-               Bull_Bear_Spread + num_prior_mergers, data = df_1)
+               Bull_Bear_Spread + num_prior_mergers + gdp_lag1_tgt, data = df_1)
 
 #TS_Consumer_Discretionary + TS_Consumer_Staples + TS_Financials + TS_Industrials + TS_Information_Technology +
 #models = list(
